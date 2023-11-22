9351d55faa69c6419cd5e5a9a1ff0a4289676647
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-11-22 15:48:21 +0400

    Add files via upload

---
+++ UML_1Lesson_Part2/UML_1Lesson_Part2/IServiceInterface.cs
@@ -0,0 +1,37 @@
+﻿using System.Xml.Schema;
+
+namespace UML_1Lesson_Part2;
+
+public interface IServiceInterface
+{
+    public void operation();
+}
+
+public class Service : IServiceInterface
+{
+    public void operation()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class Proxy : IServiceInterface
+{
+    private Service realService;
+
+    public Proxy(Service s)
+    {
+        realService = s;
+    }
+
+    public void checkAccess() 
+    {
+    
+    }
+    public void operation()
+    {
+        throw new NotImplementedException();
+    }
+
+    
+}
